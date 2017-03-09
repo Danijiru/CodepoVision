12de7329a3c6ba42e5975a016dda5ef935ea2c82
lonclesam <samuelpoirier5@gmail.com>
2017-03-09 11:34:09 +0100

    Début interface

--- CodepoVision/codepovision.cpp
+++ CodepoVision/codepovision.cpp
@@ -7,8 +7,8 @@ CodepoVision::CodepoVision(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    cv::Mat inputImage = cv::imread("/home/sam/Images/emily.jpg");
-    cv::imshow("Display Image", inputImage);
+    //cv::Mat inputImage = cv::imread("/home/sam/Images/emily.jpg");
+    //cv::imshow("Display Image", inputImage);
 
 }
 
